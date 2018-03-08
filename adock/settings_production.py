5ad99fe7e27990e6bf580348e4fd0e8b2a315bb0
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-03-08 15:44:42 +0100

    New 'template-like' settings for production

--- adock/settings_production.py
+++ adock/settings_production.py
@@ -1,3 +1,22 @@
-CORS_ORIGIN_WHITELIST = (
-    'foo.com',
+DEBUG = False
+USE_DEBUG_CONSOLE = False
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 25
+
+ADMINS = (
+    (u"Name", 'contact@example.com'),
 )
+
+MANAGERS = ADMINS
+
+SERVER_NAME = 'foo.example.com'
+SERVER_EMAIL = 'no-reply@' + SERVER_NAME
+
+CORS_ORIGIN_WHITELIST = (
+    SERVER_NAME,
+)
+ALLOWED_HOSTS = [SERVER_NAME]
+SECRET_KEY = ''
