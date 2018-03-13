0712e1cc8d6c5bfcb7e956969d597baabdf70557
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-03-13 09:20:22 +0100

    Generate SIRET from SIREN and NIC on create

--- adock/carriers/models.py
+++ adock/carriers/models.py
@@ -3,9 +3,17 @@ from django.db import models
 
 class Carrier(models.Model):
     siret = models.CharField(max_length=15, db_index=True, unique=True, editable=False)
+    siren = models.CharField(max_length=9, db_index=True, editable=False)
+    nic = models.CharField(max_length=5, editable=False)
     phone = models.CharField(max_length=10)
     email = models.EmailField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.siret
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.siret = self.siren + self.nic
+
+        super().save(*args, **kwargs)
