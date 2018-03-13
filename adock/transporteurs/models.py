27129f6bf026f0070921da4e79402fe80cf016b6
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-03-13 13:24:53 +0100

    Extend Transporteur model with GRECO/Sirène attributes

--- adock/transporteurs/models.py
+++ adock/transporteurs/models.py
@@ -12,8 +12,30 @@ class Transporteur(models.Model):
     nic = models.CharField(max_length=transporteurs_validators.NIC_LENGTH,
         editable=False,
         validators=[transporteurs_validators.validate_nic])
+    # raison_sociale in GRECO and enseigne ou l1_normalisee in Sirene
+    raison_sociale = models.CharField(max_length=38)
+    # localisation in GRECO
+    # numvoie, typevoie, libvoie in Sirene
+    adresse = models.TextField()
+    # code_postal in GRECO and codpos in Sirene
+    code_postal = models.CharField(max_length=5)
+    #
+    # telephone in GRECO used as default (changed)
     telephone = models.CharField(max_length=10, blank=True)
+    # mail in GRECO used as default (changed)
     email = models.EmailField(blank=True)
+    # dcret from Sirene
+    date_creation = models.DateField()
+    # ddebact from Sirene
+    debut_activite = models.DateField()
+    code_ape = models.CharField(max_length=5)
+    libelle_ape = models.CharField(max_length=65)
+    # TODO licenses (type + number)
+    # license_total (DN)
+    # is_above_3500_kg
+    # working_area (FRANCE or DEPARTEMENT)
+    # working_area_departments = array of zip code
+    # type_marchandise = categories from FNTR
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
