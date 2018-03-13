51885ba990eb1ff655786330ebaa702caebb5f16
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-03-13 09:54:33 +0100

    Add test for NIC validator

--- adock/carriers/tests.py
+++ adock/carriers/tests.py
@@ -5,14 +5,15 @@ from . import models as carriers_models
 
 class CarrierTestCase(TestCase):
 
-    def test_carrier_save(self):
+    def test_save(self):
         carrier = carriers_models.Carrier.objects.create(siren='123456789', nic='12345')
         self.assertEqual(carrier.siret, '12345678912345')
 
-    def test_carrier_validators(self):
-        carrier = carriers_models.Carrier.objects.create(siren='12345678', nic='12345')
+    def test_validators(self):
+        carrier = carriers_models.Carrier.objects.create(siren='12345678', nic='123')
         with self.assertRaises(ValidationError) as cm:
             carrier.full_clean()
 
         messages = cm.exception.message_dict
         self.assertEqual(messages['siren'][0], '12345678 is not a valid SIREN number')
+        self.assertEqual(messages['nic'][0], '123 is not a valid NIC number')
