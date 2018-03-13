28fc7cc2167aa54765d502b2b27c0e80c1d79652
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-03-13 15:25:57 +0100

    Factory for Transporteur with use of Faker

---
+++ adock/transporteurs/factories.py
@@ -0,0 +1,24 @@
+import datetime
+import factory
+from factory import fuzzy
+import string
+
+from . import models
+from . import validators
+
+
+class TransporteurFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Transporteur
+
+    siren = fuzzy.FuzzyText(length=validators.SIREN_LENGTH, chars=string.digits)
+    nic = fuzzy.FuzzyText(length=validators.NIC_LENGTH, chars=string.digits)
+    raison_sociale = factory.Faker('company', locale='fr_FR')
+    adresse = factory.Faker('street_address', locale='fr_FR')
+    code_postal = factory.Faker('zipcode')
+    telephone = factory.Faker('phone_number', locale='fr_FR')
+    email = factory.Faker('email', locale='fr_FR')
+    date_creation = fuzzy.FuzzyDate(datetime.date(1950, 1, 1))
+    debut_activite = factory.LazyAttribute(lambda o: o.date_creation)
+    code_ape = '4941A'
+    libelle_ape = 'Transports routiers de fret interurbains'
--- adock/transporteurs/tests.py
+++ adock/transporteurs/tests.py
@@ -1,16 +1,16 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from . import models as transporteurs_models
+from . import factories
 
 class TransporteurTestCase(TestCase):
 
     def test_save(self):
-        transporteur = transporteurs_models.Transporteur.objects.create(siren='123456789', nic='12345')
+        transporteur = factories.TransporteurFactory(siren='123456789', nic='12345')
         self.assertEqual(transporteur.siret, '12345678912345')
 
     def test_validators(self):
-        transporteur = transporteurs_models.Transporteur.objects.create(siren='12345678', nic='123')
+        transporteur = factories.TransporteurFactory(siren='12345678', nic='123')
         with self.assertRaises(ValidationError) as cm:
             transporteur.full_clean()
 
@@ -19,5 +19,5 @@ class TransporteurTestCase(TestCase):
         self.assertEqual(messages['nic'][0], "123 n'est pas un numéro NIC valide")
 
     def test_vat_number(self):
-        transporteur = transporteurs_models.Transporteur.objects.create(siren='750017097', nic='00015')
+        transporteur = factories.TransporteurFactory(siren='750017097', nic='00015')
         self.assertEqual(transporteur.get_vat_number(), 'FR18750017097')
