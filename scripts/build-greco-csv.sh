1527079387e3dd2c25bde7340c57b35b5d40c119
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-03-28 20:25:46 +0200

    idf.csv file is such a mess

--- scripts/build-greco-csv.sh
+++ scripts/build-greco-csv.sh
@@ -13,15 +13,24 @@ files=(
     Occitanie.csv
     Pays_de_la_loire.csv
     Provence_alpes_cotes_azur.csv
-    idf.csv
+#   This file isn't consistent
+#    idf-fixed.csv
 )
 
-# Replace bad original header
+# This one has a remaining '| on each line :(
+#LC_ALL=C sed -e 's/|.$//' < idf.csv > idf-fixed.csv
+
+# Replace bad original header :(
 echo '"Registre"|"No siren ou provisoire"|"Raison sociale"|"Localisation"|"Code postal"|"Ville"|"Dept"|"Telephone"|"Mail"|"Inscription activite"|"Date dernier bilan"|"Capitaux propres"|"Import finance (oui/non)"|"Regime derogatoire"|"Motif derogation"|"Type licence"|"Date fin validite"|"Copies valides"|"Copies non valides"' > greco.csv
 
+rm greco.csv
 for f in ${files[@]}; do
     # Remove each header and concat
     sed -e "1d" $f >> greco.csv
 done
+
+dos2unix greco.csv
+iconv --from-code=ISO-8859-1 --to-code=UTF-8 < greco.csv > tmp
+mv tmp greco.csv
 
 exit 0
