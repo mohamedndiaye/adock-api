3678a93161c52fe35d710445618edcd379b4efd7
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-04-04 11:13:41 +0200

    Improve test factories by using uppercase where appropriate

--- adock/transporteurs/factories.py
+++ adock/transporteurs/factories.py
@@ -1,11 +1,14 @@
 import datetime
 import factory
 from factory import fuzzy
+from faker import Faker
 import random
 import string
 
 from . import models
 from . import validators
+
+faker = Faker('fr_FR')
 
 def get_lti_number(o, n):
     keys = ['82', '84', '93']
@@ -15,23 +18,24 @@ def get_lti_number(o, n):
         n
     )
 
+
 class TransporteurFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Transporteur
 
     siret = fuzzy.FuzzyText(length=validators.SIRET_LENGTH, chars=string.digits)
     categorie_juridique = "Société par actions simplifiée (SAS)"
-    raison_sociale = factory.Faker('company', locale='fr_FR')
-    adresse = factory.Faker('street_address', locale='fr_FR')
+    raison_sociale = factory.LazyAttribute(lambda _: faker.company().upper())
+    adresse = factory.LazyAttribute(lambda _: faker.street_address().upper())
     code_postal = factory.Faker('zipcode')
-    ville = factory.Faker('city')
+    ville = factory.LazyAttribute(lambda _: faker.city().upper())
     telephone = factory.Faker('phone_number', locale='fr_FR')
     email = factory.Faker('email', locale='fr_FR')
     date_creation = fuzzy.FuzzyDate(datetime.date(1950, 1, 1))
     debut_activite = factory.LazyAttribute(lambda o: o.date_creation)
     code_ape = '4941A'
     libelle_ape = 'Transports routiers de fret interurbains'
-    gestionnaire = factory.Faker('name')
+    gestionnaire = factory.LazyAttribute(lambda _: faker.name().upper())
     working_area = models.WORKING_AREA_DEPARTEMENT
     working_area_departements = [35, 44]
     lti_numero = factory.LazyAttributeSequence(get_lti_number)
