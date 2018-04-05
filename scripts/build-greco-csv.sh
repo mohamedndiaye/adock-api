71c3d44cd99f73c6b6ccf76e7bda66243be848b2
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-04-05 23:31:35 +0200

    Include fixed IDF CSV file in GRECO import

--- scripts/build-greco-csv.sh
+++ scripts/build-greco-csv.sh
@@ -15,11 +15,12 @@ files=(
     Pays_de_la_loire.csv
     Provence_alpes_cotes_azur.csv
 #   This file isn't consistent
-#    idf-fixed.csv
+    idf-fixed.csv
 )
 
 # This one has a remaining '| on each line :(
-#LC_ALL=C sed -e 's/|.$//' < idf.csv > idf-fixed.csv
+#LC_ALL=C sed -e 's/ï¿//g' < csv/idf.csv > csv/idf-fixed.csv
+#LC_ALL=C sed -e 's/|.$//' < csv/idf.csv > csv/idf-fixed.csv
 
 # Replace bad original header :(
 echo '"Registre"|"No siren ou provisoire"|"Raison sociale"|"Localisation"|"Code postal"|"Ville"|"Dept"|"Telephone"|"Mail"|"Inscription activite"|"Date dernier bilan"|"Capitaux propres"|"Import finance (oui/non)"|"Regime derogatoire"|"Motif derogation"|"Type licence"|"Date fin validite"|"Copies valides"|"Copies non valides"' > $output
