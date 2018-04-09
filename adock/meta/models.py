a0903dc5a42cadd42ef825121eb2e8c661246c07
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-04-09 12:26:50 +0200

    New Meta model to store small and almost static information

---
+++ adock/meta/models.py
@@ -0,0 +1,14 @@
+from django.contrib.postgres.fields import JSONField
+from django.db import models
+
+
+class Meta(models.Model):
+    """Convenient models to store very small and not structured information."""
+    name = models.CharField(max_length=63, unique=True)
+    data = JSONField()
+
+    class Meta:
+        db_table = 'meta'
+
+    def __str__(self):
+        return self.name
