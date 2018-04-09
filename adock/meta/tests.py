4296e955f6005eb271de17669f2d6aed3dd749c0
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-04-09 12:27:34 +0200

    Export Meta data via the /meta endpoint

--- adock/meta/tests.py
+++ adock/meta/tests.py
@@ -1,11 +1,22 @@
+import datetime
+
 from django.urls import reverse
 from django.test import TestCase
+
+from . import models as meta_models
 
 
 class MetaTestCase(TestCase):
 
     def test_meta_index(self):
+        today_iso = str(datetime.date.today())
+        meta_models.Meta.objects.create(
+            name='transporteur',
+            data={'count': 42, 'date': today_iso}
+        )
         response = self.client.get(reverse('meta'))
         self.assertEqual(response.status_code, 200)
         data = response.json()
         self.assertEqual(data['choices']['WORKING_AREA_CHOICES']['FRANCE'], 'France')
+        self.assertEqual(data['transporteur']['count'], 42)
+        self.assertEqual(data['transporteur']['date'], today_iso)
--- adock/meta/views.py
+++ adock/meta/views.py
@@ -1,15 +1,19 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 
+from . import models as meta_models
 from ..transporteurs import models as transporteurs_models
 
 
 @cache_page(3600)
 def meta_index(request):
-    choices = {
-        'WORKING_AREA_CHOICES': dict(transporteurs_models.WORKING_AREA_CHOICES)
+    metas = meta_models.Meta.objects.all()
+    data = {
+        'choices': {
+            'WORKING_AREA_CHOICES': dict(transporteurs_models.WORKING_AREA_CHOICES),
+        }
     }
+    for meta in metas:
+        data[meta.name] = meta.data
 
-    return JsonResponse({
-        'choices': choices
-    })
+    return JsonResponse(data)
