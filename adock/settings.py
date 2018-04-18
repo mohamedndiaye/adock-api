b02c118de477b76d0cbe8df35fcdf1e95d0cf75f
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-04-18 10:16:44 +0200

    Bump version to v0.7

--- adock/settings.py
+++ adock/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 PROJECT = 'adock'
-VERSION = '0.6'
+VERSION = '0.7'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
