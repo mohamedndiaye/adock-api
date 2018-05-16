b19010a033ba46267b0463419c0d99ae75732168
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-05-16 18:11:38 +0200

    Add id_applied boolean to TransporteurScraper table

--- adock/transporteurs/models.py
+++ adock/transporteurs/models.py
@@ -164,10 +164,12 @@ class TransporteurLog(models.Model):
         db_table = 'transporteur_log'
 
 
+# FIXME Not happy with the model name, DataFiles?
 class TransporteurScraper(models.Model):
     source = models.CharField(max_length=32)
     title = models.CharField(max_length=126)
     url = models.URLField()
+    is_applied = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'transporteur_scraper'
