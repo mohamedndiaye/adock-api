0e125820f9056fb02310758f72166b416b192877
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-05-17 14:42:10 +0200

    Improve the management command to run Sirene's SQL script

--- adock/transporteurs/management/commands/sirene_update.py
+++ adock/transporteurs/management/commands/sirene_update.py
@@ -31,15 +31,28 @@ class Command(BaseCommand):
                             's:FILENAMEPLACEHOLDER:' + filename + ':g',
                             os.path.join(settings.BASE_DIR, 'scripts', 'update-sirene.sql')
                         ],
-                        stdout=subprocess.PIPE
+                        stdout=subprocess.PIPE,
                     )
                     psql_ps = subprocess.Popen(
                         [
                             'psql',
                             settings.DATABASES['default']['NAME']
                         ],
-                        stdin=sed_ps.stdin,
+                        stdin=sed_ps.stdout,
                         stdout=subprocess.PIPE
                     )
+                    # Allow sed_ps to receive SIGPIPE if psql_ps exits.
                     sed_ps.stdout.close()
-                    print(psql_ps.communicate()[0])
+                    try:
+                        # Until 6 millions of records (10 mn max)
+                        rc = psql_ps.wait(timeout=10 * 60)
+                    except subprocess.TimeoutExpired:
+                        self.stderr.write(
+                            self.style.ERROR("Timeout on running of '%s'" % filename))
+
+                    if rc == 0:
+                        self.stdout.write(
+                            self.style.SUCCESS("Filename '%s' imported with success." % filename))
+                    else:
+                        self.stderr.write(
+                            self.style.ERROR("Failed to run the SQL script 'update-sirene.sql' with the CSV file '%s'." % filename))
