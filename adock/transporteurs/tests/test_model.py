41c21722fd77c2f18beffa4169a4f51ede566f26
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-06-03 22:08:39 +0200

    Remove tests for old validators

--- adock/transporteurs/tests/test_model.py
+++ adock/transporteurs/tests/test_model.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from .. import factories
@@ -6,20 +5,6 @@ from . import test
 
 
 class TransporteurModelTestCase(TestCase):
-    def test_validators_too_short(self):
-        transporteur = factories.TransporteurFactory(siret='1234567891234')
-        with self.assertRaises(ValidationError) as cm:
-            transporteur.full_clean()
-
-        messages = cm.exception.message_dict
-        self.assertEqual(messages['siret'][0], "1234567891234 n'est pas un numéro SIRET valide")
-
-    def test_validators_not_digit(self):
-        transporteur = factories.TransporteurFactory(siret='1A345678912345')
-
-        with self.assertRaises(ValidationError):
-            transporteur.full_clean()
-
     def test_get_siren_nic(self):
         transporteur = factories.TransporteurFactory(siret=test.VALID_SIRET)
         self.assertEqual(str(transporteur), test.VALID_SIRET)
