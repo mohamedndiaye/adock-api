44ada60404496aed99e506884ea4a3e9053267f4
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-06-04 03:02:34 +0200

    Remove old import and reset scripts of transporteur

--- scripts/import-transporteur.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-psql -d adock -f reset-transporteur.sql
-../manage.py migrate && psql -d adock -f import-transporteur.sql
--- scripts/reset-transporteur.sql
+++
@@ -1,1 +0,0 @@
-drop table auth_group, auth_group_permissions, auth_permission, auth_user, auth_user_groups, auth_user_user_permissions, django_content_type, django_migrations, django_session, transporteur, transporteur_log;
