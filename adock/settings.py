6759960b1ac2e9837ace6abcd060ece17fe35b2a
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-07-19 18:22:21 +0200

    Properly set the timezone

--- adock/settings.py
+++ adock/settings.py
@@ -114,13 +114,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
