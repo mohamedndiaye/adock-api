8fb42301cc45789ef64cee33f7143002e1047d3b
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-08-28 16:17:18 +0200

    Import Objectif CO2 in transaction to be able to remove old labels

--- adock/transporteurs/management/commands/import_objectif_co2.py
+++ adock/transporteurs/management/commands/import_objectif_co2.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.core.management.base import LabelCommand
+from django.db import transaction
 from openpyxl import load_workbook
 
 from adock.transporteurs import models as transporteurs_models
@@ -15,18 +16,16 @@ class Command(LabelCommand):
         'region', 'siren', 'millesime', 'debut'
     )
 
-    def handle_label(self, xlsx_file, **options): # pylint: disable=W0221
-        self.stdout.write("Import XLSX file of labelled carriers: %s" % xlsx_file)
-
-        wb = load_workbook(filename=xlsx_file, read_only=True)
-        reader = wb.active.iter_rows()
-
-        counters = {
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.counters = {
             'siren_not_found': 0,
             'county_not_found': 0,
             'found': 0
         }
-        # raw_row are tuples (read only)
+
+    def import_objectif_co2(self, reader):
+                # raw_row are tuples (read only)
         for i, raw_row in enumerate(reader):
             # Skip header (2 lines) and trailing empty lines
             if i < 2 or raw_row[0].value is None:
@@ -52,7 +51,7 @@ class Command(LabelCommand):
                 siret__startswith=row['siren']
             )
             if not transporteurs:
-                counters['siren_not_found'] += 1
+                self.counters['siren_not_found'] += 1
                 self.stdout.write(
                     self.style.WARNING("Row %d: SIREN %s of %s not found." % (
                         i + 1, row['siren'], row['entreprise']
@@ -62,7 +61,7 @@ class Command(LabelCommand):
 
             transporteurs = transporteurs.filter(departement=row['departement'])
             if not transporteurs:
-                counters['county_not_found'] += 1
+                self.counters['county_not_found'] += 1
                 self.stdout.write(
                     self.style.WARNING("Row %d: SIREN %s and county %s of %s not found." % (
                         i + 1, row['siren'], row['departement'], row['entreprise']
@@ -78,9 +77,25 @@ class Command(LabelCommand):
                 # Same computing as in spreadsheet
                 objectif_co2_end=date_begin.replace(year=date_begin.year + 3)
             )
-            counters['found'] += 1
+            self.counters['found'] += 1
             self.stdout.write("Row %d: %s" % (i + 1, [(t.raison_sociale, t.departement) for t in transporteurs]))
 
+    def handle_label(self, xlsx_file, **options): # pylint: disable=W0221
+        self.stdout.write("Import XLSX file of labelled carriers: %s" % xlsx_file)
+
+        wb = load_workbook(filename=xlsx_file, read_only=True)
+        reader = wb.active.iter_rows()
+
+        with transaction.atomic():
+            # Reset
+            transporteurs_models.Transporteur.objects.update(
+                objectif_co2='',
+                objectif_co2_begin=None,
+                objectif_co2_end=None,
+            )
+            # Set
+            self.import_objectif_co2(reader)
+
         self.stdout.write(
-            self.style.SUCCESS("%s" % counters)
+            self.style.SUCCESS("%s" % self.counters)
         )
