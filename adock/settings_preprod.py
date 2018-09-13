d173c44ca5fdcdf00ccedd5dd0d533df69ffa6c8
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-09-13 11:07:29 +0200

    Set DB user and raven key in preprod settings

--- adock/settings_preprod.py
+++ adock/settings_preprod.py
@@ -1,9 +1,19 @@
 import os
 import raven
 
-DEBUG = True
-
 STATIC_URL = '/api/static/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adock',
+        'USER': 'deploy',
+        # For logs
+        'OPTIONS': {
+            'application_name': 'adock'
+        },
+    }
+}
 
 ADMINS = (
     (u"Stéphane Raimbault", 'stephane.raimbault@webstack.fr'),
@@ -18,7 +28,13 @@ CORS_ORIGIN_WHITELIST = (
     SERVER_NAME,
 )
 ALLOWED_HOSTS = [SERVER_NAME]
+# Web tool or ./manage.py generate_secret_key with Django extensions
 SECRET_KEY = ''
 
 # Disable mails (or listen on local port)
 EMAIL_PORT = 1025
+
+RAVEN_CONFIG = {
+    'dsn': 'https://URL-TO-PASTE-FROM-SENTRY-IO',
+    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+}
