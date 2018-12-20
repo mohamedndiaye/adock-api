c09954f3a591dc10e61467f3dbc0a9c47e980925
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-12-20 11:16:11 +0100

    Use login view of my new django-jwt-auth

--- adock/accounts/urls.py
+++ adock/accounts/urls.py
@@ -1,14 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from jwt_auth import views as jwt_auth_views
 
-from . import views
 
 urlpatterns = [
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="login.html"),
-        name="accounts_log_in",
-    ),
+    path("login/", jwt_auth_views.obtain_jwt_token, name="accounts_log_in"),
     path(
         "logout/", auth_views.LogoutView.as_view(next_page="/"), name="accounts_log_out"
     ),
