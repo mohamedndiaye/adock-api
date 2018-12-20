46e2f345a6fcb8143e04bf43b9f6ee8cecc65bc4
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-12-20 11:23:40 +0100

    No username attribute (partial revert of b7932df)

--- adock/accounts/models.py
+++ adock/accounts/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.postgres.fields import JSONField
+from django.core.mail import send_mail
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
@@ -34,12 +36,23 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     PROVIDER_CHOICES = (("AD", "A Dock"), ("FC", "France Connect"))
 
-    username = models.CharField(_("username"), max_length=255, editable=False)
-    email = models.EmailField(
-        verbose_name=_("email address"),
-        max_length=255,
-        unique=True,
+    email = models.EmailField(_("email address"), max_length=255, unique=True)
+    first_name = models.CharField(_("first name"), max_length=30, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
     )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
     provider = models.CharField(
         _("provider"), max_length=2, choices=PROVIDER_CHOICES, default="AD"
     )
@@ -48,11 +61,29 @@ class User(AbstractUser):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
 
     REQUIRED_FIELDS = []
 
-    def save(self, *args, **kwargs):
-        # We'll see later if it could be different.
-        # For now, it's convenient to submit username or email as login field
-        self.username = self.email
-        super().save(*args, **kwargs)
+    def has_perm(self, perm, obj=None):
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        # Simplest possible answer: Yes, always
+        return True
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
