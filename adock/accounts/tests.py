9965826067a2301b7110dec4e012af8e1db0789f
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-01-10 02:07:27 +0100

    Major improvements and unit tests for France Connect views

--- adock/accounts/tests.py
+++ adock/accounts/tests.py
@@ -1,7 +1,13 @@
+import requests_mock
+
+from django.conf import settings
 from django.test import TestCase
+from django.urls import reverse
 
 from . import factories as accounts_factories
 from . import jwt as accounts_jwt
+from . import models as accounts_models
+from . import views as accounts_views
 
 
 class AccountsTestCase(TestCase):
@@ -16,3 +22,120 @@ class AccountsTestCase(TestCase):
         user = accounts_factories.UserFactory()
         jwt_payload = accounts_jwt.jwt_payload_handler(user)
         self.assertEqual(jwt_payload["user_id"], user.pk)
+
+    def test_france_connect_authorize(self):
+        response = self.client.get(reverse("france_connect_authorize"))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            response.url.startswith(settings.FRANCE_CONNECT_URLS["authorize"])
+        )
+
+
+class TestCreateOrUpdateUserTestCase(TestCase):
+    def test_create_user(self):
+        # Other not related user (no sub)
+        accounts_factories.UserFactory(email="other@example.com")
+
+        user_infos = {"email": "bar@example.com", "first_name": "Roger", "sub": "12345"}
+
+        # The other_user has a different sub number so create a user
+        user, created = accounts_views.create_or_update_user(user_infos)
+        self.assertTrue(created)
+        self.assertEqual(user.email, user_infos["email"])
+        self.assertEqual(user.provider, accounts_models.PROVIDER_FRANCE_CONNECT)
+        self.assertEqual(user.provider_data["sub"], user_infos["sub"])
+
+    def test_update_user(self):
+        accounts_factories.UserFactory(
+            email="foo@example.com",
+            provider=accounts_models.PROVIDER_FRANCE_CONNECT,
+            provider_data={"sub": "12345"},
+        )
+        # Update an existing user
+        user_infos = {"email": "bar@example.com", "first_name": "Roger", "sub": "12345"}
+        user, created = accounts_views.create_or_update_user(user_infos)
+        self.assertFalse(created)
+        self.assertEqual(user.email, user_infos["email"])
+
+    def test_creating_conflicting_user(self):
+        accounts_factories.UserFactory(email="other@example.com")
+
+        # Try to use an existing email
+        user_infos = {
+            "email": "other@example.com",
+            "first_name": "Roger",
+            "sub": "12345",
+        }
+        user, created = accounts_views.create_or_update_user(user_infos)
+        self.assertIsNone(user)
+        self.assertFalse(created)
+
+    def test_updating_conflicting_user(self):
+        accounts_factories.UserFactory(email="someone@example.com")
+        accounts_factories.UserFactory(
+            email="joe@example.com",
+            provider=accounts_models.PROVIDER_FRANCE_CONNECT,
+            provider_data={"sub": "12345"},
+        )
+        user_infos = {
+            "email": "someone@example.com",
+            "first_name": "Roger",
+            "sub": "12345",
+        }
+        user, created = accounts_views.create_or_update_user(user_infos)
+        self.assertFalse(created)
+        # Unable to refresh_from_db with a error in current transaction
+        # so the instance attributes are different in DB...
+        self.assertEqual(user.email, user_infos["email"])
+
+
+class FranceConnectCallbackTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse("france_connect_callback")
+
+    def test_unable_to_get_token(self):
+        with requests_mock.mock() as m:
+            m.post(settings.FRANCE_CONNECT_URLS["token"], status_code=404)
+            response = self.client.get(self.url, {"code": "007"})
+        self.assertEqual(response.status_code, 404)
+        payload = response.json()
+        self.assertEqual(
+            payload["message"], "Unable to get the token from France Connect."
+        )
+
+    def test_unable_to_get_user_infos(self):
+        with requests_mock.mock() as m:
+            m.post(
+                settings.FRANCE_CONNECT_URLS["token"],
+                status_code=200,
+                json={"access_token": "123456789"},
+            )
+            m.get(settings.FRANCE_CONNECT_URLS["userinfo"], status_code=404)
+            response = self.client.get(self.url, {"code": "007"})
+        self.assertEqual(response.status_code, 404)
+        payload = response.json()
+        self.assertEqual(
+            payload["message"], "Unable to get the user infos from France Connect."
+        )
+
+    def test_create_user(self):
+        with requests_mock.mock() as m:
+            m.post(
+                settings.FRANCE_CONNECT_URLS["token"],
+                status_code=200,
+                json={"access_token": "123456789"},
+            )
+            m.get(
+                settings.FRANCE_CONNECT_URLS["userinfo"],
+                status_code=200,
+                json={
+                    "email": "joe@example.com",
+                    "sub": "1234",
+                    "last_name": "MARTIN",
+                    "birthdate": "1967-05-23",
+                },
+            )
+            response = self.client.get(self.url, {"code": "007"})
+        self.assertEqual(response.status_code, 200)
+        payload = response.json()
+        self.assertEqual(payload["message"], "OK")
--- adock/accounts/views.py
+++ adock/accounts/views.py
@@ -1,7 +1,11 @@
+import json
+import logging
 import requests
 
 from django.conf import settings
+from django.db import IntegrityError
 from django.http import HttpResponseRedirect, JsonResponse
+from django.utils.crypto import get_random_string
 from django.utils.http import urlencode
 from django.views.decorators.http import require_GET, require_POST
 import sentry_sdk
@@ -29,6 +33,49 @@ def france_connect_authorize(request):
     )
 
 
+def create_or_update_user(user_infos):
+    # FIXME email field is not provided by integration platform!
+    if "email" not in user_infos:
+        logger.error("No email field")
+        user_infos["email"] = get_random_string(length=32) + "@example.com"
+
+    if "address" in user_infos and "formatted" in user_infos["address"]:
+        user_infos["address"] = user_infos["address"]["formatted"]
+
+    try:
+        user, created = accounts_models.User.objects.get_or_create(
+            provider=accounts_models.PROVIDER_FRANCE_CONNECT,
+            provider_data__sub=user_infos["sub"],
+            defaults={
+                "email": user_infos["email"],
+                "first_name": user_infos.get("given_name", ""),
+                "last_name": user_infos.get("family_name", ""),
+                "provider": accounts_models.PROVIDER_FRANCE_CONNECT,
+                "provider_data": user_infos,
+            },
+        )
+    except IntegrityError as e:
+        logger.error("Unable to create the user: %s", e.__cause__)
+        sentry_sdk.capture_exception(e)
+        return None, False
+
+    if not created:
+        # Forced update (may be it will possible to set a different email later)
+        user.email = user_infos.get("email", "")
+        user.first_name = user_infos.get("given_name", "")
+        user.family_name = user_infos.get("family_name", "")
+        user.provider_data = user_infos
+        try:
+            user.save()
+        except IntegrityError as e:
+            # Don't update the fields
+            logger.error("Unable to update the user: %s", e.__cause__)
+            sentry_sdk.capture_exception(e)
+            # Take care to refresh the user...
+
+    return user, created
+
+
 @require_GET
 def france_connect_callback(request):
     # state is also available and should be checked (#1)
@@ -42,26 +89,35 @@ def france_connect_callback(request):
         "redirect_uri": settings.FRANCE_CONNECT_URL_CALLBACK,
     }
     response = requests.post(settings.FRANCE_CONNECT_URLS["token"], data=data)
+
     if response.status_code == 200:
         # Contains access_token, token_type, expires_in, id_token
         # id_token contains a field 'nonce' to check (#2)
         token_data = response.json()
         # A token has been provided so it's time to fetch associated user infos
         # because the token is only valid for 5 seconds.
-        requests.get(
+        response = requests.get(
             settings.FRANCE_CONNECT_URLS["userinfo"],
             params={"schema": "openid"},
-            headers={
-                "headers": {"Authorization": "Bearer " + token_data["access_token"]}
-            },
+            headers={"Authorization": "Bearer " + token_data["access_token"]},
         )
-        # given_name
-        # family_name
-        # birthdate
-        # gender
-        # birthplace
-        # birthcountry
-        # FIXME Store in DB?
+        if response.status_code == 200:
+            user_infos = json.loads(response.content.decode("utf-8"))
+            user, created = create_or_update_user(user_infos)
+            if created:
+                logger.info("New user created '%s'.", user.email)
+            return JsonResponse({"message": "OK"})
+        else:
+            message = "Unable to get the user infos from France Connect."
+            logger.error(message)
+            sentry_sdk.capture_message(message)
+            return JsonResponse({"message": message}, status=response.status_code)
+    else:
+        message = "Unable to get the token from France Connect."
+        logger.error(message)
+        sentry_sdk.capture_message("%s\n%s" % (message, response.content))
+        # The response is certainly ignored by FC but it's convenient for our tests
+        return JsonResponse({"message": message}, status=response.status_code)
 
 
 @require_POST
