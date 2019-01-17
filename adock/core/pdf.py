2290dcfb592a9e0b16739e0efb770d465a7ec897
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-01-17 11:16:26 +0100

    Cleanup PDF generation
    - no search/replace of existing URLs
    - remove PDF on disk
    - default directory for tmp

--- adock/core/pdf.py
+++ adock/core/pdf.py
@@ -10,22 +10,11 @@ logger = logging.getLogger(__name__)
 
 
 def pdf_response(response, pdf_filename):
-    dest_dir = os.path.join(settings.MEDIA_ROOT, "tmp")
-    if not os.access(dest_dir, os.W_OK):
-        logger.info("Unable to write in %s, trying to create it...", dest_dir)
-        os.mkdir(dest_dir)
-
     (_, html_filename) = tempfile.mkstemp(
-        prefix=pdf_filename.replace(".pdf", ""), suffix=".html", dir=dest_dir
+        prefix=pdf_filename.replace(".pdf", ""), suffix=".html"
     )
     with open(html_filename, "w+") as f:
         content = response.content.decode()
-        content = content.replace(
-            'href="%s' % settings.STATIC_URL, 'href="%s/' % settings.STATIC_ROOT
-        )
-        content = content.replace(
-            'src="%s' % settings.MEDIA_URL, 'src="%s/' % settings.MEDIA_ROOT
-        )
         f.write(content)
 
     ps = subprocess.Popen(
@@ -43,4 +32,6 @@ def pdf_response(response, pdf_filename):
     with open(pdf_filename, "rb") as f:
         response = HttpResponse(f.read(), content_type="application/pdf")
         response["Content-Disposition"] = 'attachment; filename="%s"' % (pdf_filename)
+
+    os.remove(pdf_filename)
     return response
