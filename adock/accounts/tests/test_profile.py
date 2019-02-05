24fd07e528510e712a721c18d49d12fbe57d3567
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-02-05 09:20:36 +0100

    Add unit tests for profile

---
+++ adock/accounts/tests/test_profile.py
@@ -0,0 +1,27 @@
+from django.urls import reverse
+
+from adock.accounts.test import AuthTestCaseBase
+
+from adock.carriers import factories as carriers_factories
+
+
+class ProfileTestCase(AuthTestCaseBase):
+    def setUp(self):
+        self.url = reverse("accounts_profile")
+        super().setUp()
+
+    def test_anonymous(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_profile(self):
+        carrier = carriers_factories.CarrierFactory()
+        carrier.add_owner(self.user)
+
+        http_authorization = self.log_in()
+        response = self.client.get(self.url, HTTP_AUTHORIZATION=http_authorization)
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(self.user.last_login, data["user"]["last_login"])
+        self.assertEqual(self.user.provider, data["user"]["provider"])
+        self.assertEqual(carrier.siret, data["user"]["carriers"][0]["siret"])
