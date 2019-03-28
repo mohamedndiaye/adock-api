74b0b6e61b2360955263e92bc8dfbf450fdb2d4b
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-03-28 15:05:42 +0100

    Bump version: 1.9.4 → 2.0.0

--- adock/settings.py
+++ adock/settings.py
@@ -15,7 +15,7 @@ import datetime
 import os
 
 PROJECT = "adock"
-VERSION = "1.9.4"
+VERSION = "2.0.0"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
