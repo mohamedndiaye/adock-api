1f639043a8f03f96fdb564803cd67ed428b4741f
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-04-03 19:41:12 +0200

    Rewrite flaky test on stats

--- adock/stats/tests.py
+++ adock/stats/tests.py
@@ -1,5 +1,6 @@
 from unittest import skipIf
 import datetime
+import pytz
 import random
 
 from django.conf import settings
@@ -43,11 +44,20 @@ class StatsTestCase(AuthTestCase):
         # One carrier not confirmed
         carriers_factories.CarrierFactory(with_editable=True)
 
+        # 3 carriers confirmed over the n previous month
+        now = timezone.now()
+        paris_tz = pytz.timezone("Europe/Paris")
+        first_day_of_current_month = datetime.datetime(
+            year=now.year, month=now.month, day=1
+        )
+        # Approx first day 6 month ago (no datedelta dependency)
+        start = paris_tz.localize(
+            first_day_of_current_month - datetime.timedelta(days=STATS_NB_MONTHS * 30)
+        )
         for _ in range(3):
-            confirmed_at = (
-                timezone.now()
-                - datetime.timedelta(days=random.randint(1, STATS_NB_MONTHS - 1) * 31)
-            ).replace(day=1)
+            confirmed_at = start + datetime.timedelta(
+                days=random.randint(1, STATS_NB_MONTHS * 30)
+            )
             carriers_factories.CarrierFactory(
                 with_editable={"confirmed_at": confirmed_at}
             )
@@ -69,6 +79,7 @@ class StatsTestCase(AuthTestCase):
         self.assertEqual(response.status_code, 200)
         stats = response.json()
 
+        # Total (outside last n months included)
         self.assertEqual(stats["modified_carriers"], 4)
 
         # 6 months
