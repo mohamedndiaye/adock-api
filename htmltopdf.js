95f85b659593e2cfb3acb44e23223531aee1b1ff
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-05-10 07:55:00 +0200

    Add waitUntil to htmltopdf

--- htmltopdf.js
+++ htmltopdf.js
@@ -1,20 +1,24 @@
-const fs = require('fs');
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
   const htmlFilename = await process.argv[2];
   const pdfFilename = await process.argv[3];
   const browser = await puppeteer.launch({
-    args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox', '--allow-file-access-from-files']
+    args: [
+      "--disable-gpu",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--allow-file-access-from-files"
+    ]
   });
   const page = await browser.newPage();
-  page.on('console', (...args) => logger.info('PAGE LOG:', ...args));
-  page.on('error', (err) => {
+  page.on("console", (...args) => logger.info("PAGE LOG:", ...args));
+  page.on("error", err => {
     logger.error(`Error event emitted: ${err}`);
     logger.error(err.stack);
-  })
-  await page.emulateMedia('screen');
-  await page.goto('file://' + htmlFilename);
-  await page.pdf({path: pdfFilename, format: 'A4'});
+  });
+  await page.emulateMedia("screen");
+  await page.goto("file://" + htmlFilename, { waitUntil: "networkidle0" });
+  await page.pdf({ path: pdfFilename, format: "A4" });
   await browser.close();
 })();
